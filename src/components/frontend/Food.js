c126440ec12794d6a79ce05f5c3a7102e0d6ff2d
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2023-03-24 12:50:00 +0600

    category complete

--- src/components/frontend/Food.js
+++ src/components/frontend/Food.js
@@ -4,8 +4,6 @@ import { TopNav } from '../../layouts/frontend/TopNav';
 import { Goods } from './card/Goods';
 import { Footer } from '../../layouts/frontend/Footer';
 import { BottomFix } from './BottomFix';
-import { postPublicApi } from "../../api/apiCall";
-import Pagination from 'react-js-pagination';
 import { useDispatch, useSelector } from 'react-redux';
 import { catWiseFilter } from '../../redux/action/action';
 import Spinner from './Spinner/Spinner';
@@ -16,31 +14,23 @@ export const Food = () => {
     const getData = useSelector((state) => state.catId);
     const [categoryList, setcategoryList] = useState([]);
     const [itemList, setitemList] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalItem, setTotalItem] = useState(1);
-    const [itemPerPage, setItemPerPage] = useState(1);
     const dispatch = useDispatch();
 
     const [formValue, setFormValue] = useState({});
 
-    const getAllFoods = async (page = 1) => {
-        setLoading(true);
-        const response = await postPublicApi("item-list", { page: page, pagination_num: 40 });
-        if (response) {
-          if (response.code === 200) {
-            setitemList(response?.data?.data);
-            setCurrentPage(response?.data?.current_page);
-                setTotalItem(response?.data?.total);
-                setItemPerPage(response?.data?.per_page);
-                setLoading(false);
-          } else {
-          }
-        }
-      };
-    
-      useEffect(() => {
-        getAllFoods();
-      }, []);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`Data/products.json`)
+     .then((response) => response.json())
+     .then((actualData) => setitemList(actualData))
+     
+     .catch((err) => {
+      console.log(err.message);
+      
+     });
+     setLoading(false);
+   }, []);
+
 
     const InputValue = (e) => {
         const name = e.target.name;
@@ -49,50 +39,70 @@ export const Food = () => {
         setFormValue(data)
     }
 
-    const getCategory = async (page = 1) => {
-        const url = `category-list`;
-        setLoading(true);
-        const response = await postPublicApi(url, { pagination_num: 100 });
-        if (response) {
-            if (response.code === 200) {
-                setcategoryList(response?.data?.data);
-                setLoading(false);
-            } else {
-            }
-        }
-    };
-
-    const getItem = async (page = 1) => {
-        const url = `item-list?page=${page}`;
-        setLoading(true);
-        const response = await postPublicApi(url, { ...formValue, pagination_num: 40,cat_id:getData.cat_id });
-        if (response) {
-            if (response.code === 200) {
-                setitemList(response?.data?.data);
-                setCurrentPage(response?.data?.current_page);
-                setTotalItem(response?.data?.total);
-                setItemPerPage(response?.data?.per_page);
-                setLoading(false);
-            } else {
-            }
-        }
-    };
-
-    const handleFilter = (e) => {
-        e.preventDefault();
-        dispatch(catWiseFilter({cat_id:null}))
-        getItem()
-    };
-
-    const resetFilterOptions = (e) => {
-        setFormValue({})
-        document.getElementById("filter_form").reset();
-    }
-
     useEffect(() => {
-        getItem();
-        getCategory();
-    }, [getData]);
+        fetch(`Data/categories.json`)
+          .then((response) => response.json())
+          .then((actualData) => setcategoryList(actualData))
+          .catch((err) => {
+            console.log(err.message);
+          });
+      }, []);
+
+    // const handleFilter = (e) => {
+    //     e.preventDefault();
+    //     dispatch(catWiseFilter({cat_id:null}))
+    //     // getItem()
+    // };
+
+    // const resetFilterOptions = (e) => {
+    //     setFormValue({})
+    //     document.getElementById("filter_form").reset();
+    // }
+
+    const [searchTerm, setSearchTerm] = useState("");
+const [selectedCategory, setSelectedCategory] = useState("");
+
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+      };
+      
+      const handleCategoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+      };
+      
+      const filteredItems = itemList.filter((item) => {
+        // filter by category
+        if (selectedCategory && item.cat_id !== selectedCategory) {
+          return false;
+        }
+        // filter by search term
+        if (searchTerm && !item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return false;
+        }
+        return true;
+      });
+      
+      const handleFilter = (event) => {
+        event.preventDefault();
+        // filter the itemList based on search term and selected category
+        const filteredItems = itemList.filter((item) => {
+          if (selectedCategory && item.cat_id !== selectedCategory) {
+            return false;
+          }
+          if (searchTerm && !item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return false;
+          }
+          return true;
+        });
+        setitemList(filteredItems);
+      };
+      
+      const resetFilterOptions = () => {
+        setSelectedCategory("");
+        setSearchTerm("");
+        setitemList(itemList);
+      };
+
 
     return (
         <div>
@@ -108,36 +118,56 @@ export const Food = () => {
                             <h1 className='fw-bold mb-0 color-1 ms-md-5 mt-1'>Our Foods</h1>
                         </div>
                         <div className='col-md-8 mt-3'>
-                            <form onChange={InputValue} onSubmit={handleFilter} id="filter_form">
-                                <div className='row'>
-                                    <div className='col-md-4'>
-                                        <div className="input-group mb-3">
-                                            <input name='name' style={{ fontSize: '18px', borderColor: '#C64A24' }} type="text" className="form-control color-1" placeholder="Food Name" aria-label="Search your menu category" aria-describedby="basic-addon2" />
+                        <form id="filter_form">
+                            <div className='row'>
+                                        <div className='col-md-4'>
+                                            <div className="input-group mb-3">
+                                            <input
+                                                name='name'
+                                                style={{ fontSize: '18px', borderColor: '#C64A24' }}
+                                                type="text"
+                                                className="form-control color-1"
+                                                placeholder="Food Name"
+                                                aria-label="Search your menu category"
+                                                aria-describedby="basic-addon2"
+                                                value={searchTerm}
+                                                onChange={handleSearchTermChange}
+                                            />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className='col-md-4'>
-                                        <div className="input-group mb-3">
-                                            <select name='filter_cat_id' style={{ fontSize: '18px', borderColor: '#C64A24' }} className="form-control" aria-label="Default select example"
+                                        <div className='col-md-4'>
+                                            <div className="input-group mb-3">
+                                            <select
+                                                name='filter_cat_id'
+                                                style={{ fontSize: '18px', borderColor: '#C64A24' }}
+                                                className="form-control"
+                                                aria-label="Default select example"
+                                                value={selectedCategory}
+                                                onChange={handleCategoryChange}
                                             >
                                                 <option defaultValue={""} value={""}>Select Category</option>
-
                                                 {categoryList?.map((item) => {
-                                                    return (
-                                                        <option key={item?.id} value={item?.id}
-                                                        selected={getData?.cat_id === item.id? 'selected' : null}
-                                                        >{item?.cat_name}</option>
-                                                    )
+                                                return (
+                                                    <option
+                                                    key={item?.id}
+                                                    value={item?.id}
+                                                    selected={getData?.cat_id === item.id? 'selected' : null}
+                                                    >
+                                                    {item?.cat_name}
+                                                    </option>
+                                                )
                                                 })}
                                             </select>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className='col-md-4'>
+                                        <div className='col-md-4'>
                                         <button type='submit' className='btn bg-color-1 text-light me-2' style={{ fontSize: '18px', borderColor: '#C64A24' }}>
                                             Filter</button>
                                         <button type='submit' onClick={resetFilterOptions} className='btn bg-color-1 text-light ms-2' style={{ fontSize: '18px', borderColor: '#C64A24' }}>Clear Filter</button>
                                     </div>
                                 </div>
                             </form>
+                            
                         </div>
                     </div>
                 </div>
@@ -153,7 +183,7 @@ export const Food = () => {
                         )
                     })}
 
-                    <div>
+                    {/* <div>
                         {(itemPerPage < totalItem) &&
                             <div className='mt-5 mx-5'>
                                 <Pagination
@@ -167,7 +197,7 @@ export const Food = () => {
                             </div>
                         }
                         
-                    </div>
+                    </div> */}
 
 
                 </div>
